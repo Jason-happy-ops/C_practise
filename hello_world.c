1f120d1147d65cff528e876228228953f7bba099
Jason-happy-ops <2941854208@qq.com>
2026-02-16 10:51:48 +0800

    hello world

---
+++ hello_world.c
@@ -0,0 +1,7 @@
+// 最简单的C语言程序：输出Hello World
+#include <stdio.h>  // 引入标准输入输出库（必须）
+
+int main() {         // 程序入口函数（所有C程序都要有main函数）
+    printf("Hello World!\n");  // 输出字符串，\n是换行符
+    return 0;        // 程序正常结束，返回0
+}
